22240681692885fbfb06b3bb96ec03c3530b6d12
fizz-whu <fizz.whu@gmail.com>
2017-10-30 11:13:42 -0500

    init leetProject

---
+++ src/main/java/leet/fb/general/App2.java
@@ -0,0 +1,11 @@
+package leet.fb.general;
+
+import java.util.*;
+import java.lang.*;
+
+public class App2 {
+
+  public String printString() {
+    return "helloApp2";
+  }
+}
---
+++ src/test/java/leet/fb/general/App2Test.java
@@ -0,0 +1,12 @@
+package leet.fb.general;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class App2Test {
+  @Test
+  public void testPrintString() {
+    App2 app2Test = new App2();
+    assertEquals("helloApp2", app2Test.printString());
+  }
+}
